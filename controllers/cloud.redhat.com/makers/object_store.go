9de07471d5c39638b0e7edf6bac40cea04e41349
Pete Savage <psavage@redhat.com>
2020-09-16 14:25:23 +0100

    Split base makers

--- controllers/cloud.redhat.com/makers/object_store.go
+++ controllers/cloud.redhat.com/makers/object_store.go
@@ -17,10 +17,21 @@ package makers
 import (
 	//config "github.com/redhatinsights/app-common-go/pkg/api/v1" - to replace the import below at a future date
 
+	"context"
+	"fmt"
+
+	crd "cloud.redhat.com/whippoorwill/v2/apis/cloud.redhat.com/v1alpha1"
 	"cloud.redhat.com/whippoorwill/v2/controllers/cloud.redhat.com/config"
+	"cloud.redhat.com/whippoorwill/v2/controllers/cloud.redhat.com/utils"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
+	apps "k8s.io/api/apps/v1"
+	core "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 //ObjectStoreMaker makes the StorageConfig object
@@ -90,5 +101,150 @@ func (obs *ObjectStoreMaker) minio() error {
 		obs.config.Endpoint = endpoint
 	}
 
+	return nil
+}
+
+func MakeMinio(client client.Client, ctx context.Context, req ctrl.Request, base *crd.InsightsBase) error {
+	minioObjName := fmt.Sprintf("%v-minio", req.Name)
+	minioNamespacedName := types.NamespacedName{
+		Namespace: req.Namespace,
+		Name:      minioObjName,
+	}
+
+	dd := apps.Deployment{}
+	err := client.Get(ctx, minioNamespacedName, &dd)
+	update, err := utils.UpdateOrErr(err)
+
+	if err != nil {
+		return err
+	}
+
+	labels := base.GetLabels()
+	labels["base-app"] = minioNamespacedName.Name
+
+	dd.SetName(minioNamespacedName.Name)
+	dd.SetNamespace(minioNamespacedName.Namespace)
+	dd.SetLabels(labels)
+	dd.SetOwnerReferences([]metav1.OwnerReference{base.MakeOwnerReference()})
+
+	replicas := int32(1)
+
+	dd.Spec.Replicas = &replicas
+	dd.Spec.Selector = &metav1.LabelSelector{MatchLabels: labels}
+	dd.Spec.Template.Spec.Volumes = []core.Volume{{
+		Name: minioNamespacedName.Name,
+		VolumeSource: core.VolumeSource{
+			PersistentVolumeClaim: &core.PersistentVolumeClaimVolumeSource{
+				ClaimName: minioNamespacedName.Name,
+			},
+		}},
+	}
+	dd.Spec.Template.ObjectMeta.Labels = labels
+
+	pullSecretRef := core.LocalObjectReference{Name: "quay-cloudservices-pull"}
+	dd.Spec.Template.Spec.ImagePullSecrets = []core.LocalObjectReference{pullSecretRef}
+
+	var accessKey, secretKey core.EnvVar
+	if !update {
+		accessKey = core.EnvVar{Name: "MINIO_ACCESS_KEY", Value: utils.RandString(12)}
+		secretKey = core.EnvVar{Name: "MINIO_SECRET_KEY", Value: utils.RandString(12)}
+		annotations := base.GetAnnotations()
+		if annotations == nil {
+			annotations = make(map[string]string)
+		}
+		annotations["accessKey"] = accessKey.Value
+		annotations["secretKey"] = secretKey.Value
+		annotations["endpoint"] = fmt.Sprintf("%v.%v.svc:9000", minioObjName, req.Namespace)
+		base.SetAnnotations(annotations)
+		if err = client.Update(ctx, base); err != nil {
+			return err
+		}
+	} else {
+		appConfig := base.GetAnnotations()
+		if err != nil {
+			return err
+		}
+		accessKey = core.EnvVar{Name: "MINIO_ACCESS_KEY", Value: appConfig["accessKey"]}
+		secretKey = core.EnvVar{Name: "MINIO_SECRET_KEY", Value: appConfig["secretKey"]}
+	}
+	envVars := []core.EnvVar{accessKey, secretKey}
+	ports := []core.ContainerPort{
+		{
+			Name:          "minio",
+			ContainerPort: 9000,
+		},
+	}
+
+	// TODO Readiness and Liveness probes
+
+	c := core.Container{
+		Name:  minioNamespacedName.Name,
+		Image: "minio/minio",
+		Env:   envVars,
+		Ports: ports,
+		VolumeMounts: []core.VolumeMount{{
+			Name:      minioNamespacedName.Name,
+			MountPath: "/storage",
+		}},
+		Args: []string{
+			"server",
+			"/storage",
+		},
+	}
+
+	dd.Spec.Template.Spec.Containers = []core.Container{c}
+	dd.Spec.Template.SetLabels(labels)
+
+	if err = update.Apply(ctx, client, &dd); err != nil {
+		return err
+	}
+
+	s := core.Service{}
+	err = client.Get(ctx, minioNamespacedName, &s)
+
+	update, err = utils.UpdateOrErr(err)
+	if err != nil {
+		return err
+	}
+
+	servicePorts := []core.ServicePort{}
+	minioPort := core.ServicePort{Name: "minio", Port: 9000, Protocol: "TCP"}
+	servicePorts = append(servicePorts, minioPort)
+
+	s.SetName(minioNamespacedName.Name)
+	s.SetNamespace(minioNamespacedName.Namespace)
+	s.SetLabels(labels)
+	s.SetOwnerReferences([]metav1.OwnerReference{base.MakeOwnerReference()})
+
+	s.Spec.Selector = labels
+	s.Spec.Ports = servicePorts
+
+	if err = update.Apply(ctx, client, &s); err != nil {
+		return err
+	}
+
+	pvc := core.PersistentVolumeClaim{}
+
+	err = client.Get(ctx, minioNamespacedName, &pvc)
+
+	update, err = utils.UpdateOrErr(err)
+	if err != nil {
+		return err
+	}
+
+	pvc.SetName(minioNamespacedName.Name)
+	pvc.SetNamespace(minioNamespacedName.Namespace)
+	pvc.SetLabels(labels)
+	pvc.SetOwnerReferences([]metav1.OwnerReference{base.MakeOwnerReference()})
+	pvc.Spec.AccessModes = []core.PersistentVolumeAccessMode{core.ReadWriteOnce}
+	pvc.Spec.Resources = core.ResourceRequirements{
+		Requests: core.ResourceList{
+			core.ResourceName(core.ResourceStorage): resource.MustParse("1Gi"),
+		},
+	}
+
+	if err = update.Apply(ctx, client, &pvc); err != nil {
+		return err
+	}
 	return nil
 }
