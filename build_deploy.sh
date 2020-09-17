adeaff706b995130a1236bd74aef672fe9e151e2
Kyle Lape <klape@redhat.com>
2020-09-17 13:35:39 -0500

    Add build scripts and deployment template

---
+++ build_deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -exv
+
+IMAGE="quay.io/cloudservices/clowder"
+IMAGE_TAG=$(git rev-parse --short=7 HEAD)
+
+if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
+    echo "QUAY_USER and QUAY_TOKEN must be set"
+    exit 1
+fi
+
+DOCKER_CONF="$PWD/.docker"
+mkdir -p "$DOCKER_CONF"
+docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+docker --config="$DOCKER_CONF" build -t "${IMAGE}:${IMAGE_TAG}" .
+docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
