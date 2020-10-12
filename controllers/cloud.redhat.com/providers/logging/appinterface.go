33ea52f0ef4b7485334a8ff548a11d51e2431973
Kyle Lape <klape@redhat.com>
2020-10-12 16:39:52 -0500

    No need to base64 decode secret data

--- controllers/cloud.redhat.com/providers/logging/appinterface.go
+++ controllers/cloud.redhat.com/providers/logging/appinterface.go
@@ -1,13 +1,10 @@
 package logging
 
 import (
-	"fmt"
-
 	crd "cloud.redhat.com/clowder/v2/apis/cloud.redhat.com/v1alpha1"
 	"cloud.redhat.com/clowder/v2/controllers/cloud.redhat.com/config"
 	"cloud.redhat.com/clowder/v2/controllers/cloud.redhat.com/errors"
 	p "cloud.redhat.com/clowder/v2/controllers/cloud.redhat.com/providers"
-	"cloud.redhat.com/clowder/v2/controllers/cloud.redhat.com/utils"
 	core "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
 )
@@ -40,35 +37,17 @@ func setCloudwatchSecret(ns string, p *p.Provider, c *config.LoggingConfig) erro
 	}
 
 	secret := core.Secret{}
-	fmt.Printf("%+v\n", p)
 	err := p.Client.Get(p.Ctx, name, &secret)
 
 	if err != nil {
 		return errors.Wrap("Failed to fetch cloudwatch secret", err)
 	}
 
-	cwKeys := []string{
-		"aws_access_key_id",
-		"aws_secret_access_key",
-		"aws_region",
-		"log_group_name",
-	}
-
-	decoded := make([]string, 4)
-
-	for i := 0; i < 4; i++ {
-		decoded[i], err = utils.B64Decode(&secret, cwKeys[i])
-
-		if err != nil {
-			return errors.Wrap("Failed to b64 decode", err)
-		}
-	}
-
 	c.Cloudwatch = &config.CloudWatchConfig{
-		AccessKeyId:     decoded[0],
-		SecretAccessKey: decoded[1],
-		Region:          decoded[2],
-		LogGroup:        decoded[3],
+		AccessKeyId:     string(secret.Data["aws_access_key_id"]),
+		SecretAccessKey: string(secret.Data["aws_secret_access_key"]),
+		Region:          string(secret.Data["aws_region"]),
+		LogGroup:        string(secret.Data["log_group_name"]),
 	}
 
 	return nil
