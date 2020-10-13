db88671b0f5763d8941dc3b204c0f2a4d7dda02a
Kyle Lape <klape@redhat.com>
2020-10-13 16:38:19 -0500

    Add strimzi types to CSV rbac role

--- controllers/cloud.redhat.com/clowdapp_controller.go
+++ controllers/cloud.redhat.com/clowdapp_controller.go
@@ -45,9 +45,10 @@ type ClowdAppReconciler struct {
 
 // +kubebuilder:rbac:groups=cloud.redhat.com,resources=clowdapps,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=cloud.redhat.com,resources=clowdapps/status,verbs=get;update;patch
-
 // +kubebuilder:rbac:groups="",resources=services;persistentvolumeclaims;secrets,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=kafka.strimzi.io,resources=kafkatopic,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=kafka.strimzi.io,resources=kafka,verbs=get;list;watch
 
 // Reconcile fn
 func (r *ClowdAppReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
