bd0a500ccbb559f7b563f33bdd591aa1a7e579d1
Kyle Lape <klape@redhat.com>
2020-12-02 20:54:59 -0600

    Refactor status code to DRY it up a bit

--- controllers/cloud.redhat.com/object/object.go
+++ controllers/cloud.redhat.com/object/object.go
@@ -1,12 +1,17 @@
 package object
 
 import (
+	"cloud.redhat.com/clowder/v2/apis/cloud.redhat.com/v1alpha1/common"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
+// ClowdObject is used to be able to treat ClowdEnv and ClowdApp as the same type
 type ClowdObject interface {
 	MakeOwnerReference() metav1.OwnerReference
 	GetLabels() map[string]string
 	GetClowdNamespace() string
 	GetClowdName() string
+	GetUID() types.UID
+	GetDeploymentStatus() *common.DeploymentStatus
 }
---
+++ controllers/cloud.redhat.com/status.go
@@ -0,0 +1,49 @@
+package controllers
+
+import (
+	"context"
+
+	"cloud.redhat.com/clowder/v2/controllers/cloud.redhat.com/object"
+	"cloud.redhat.com/clowder/v2/controllers/cloud.redhat.com/utils"
+	apps "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func filterOwnedDeployments(deploymentList *apps.DeploymentList, uid types.UID) {
+	depList := []apps.Deployment{}
+	for _, deployment := range deploymentList.Items {
+		for _, owner := range deployment.ObjectMeta.OwnerReferences {
+			if owner.UID == uid {
+				depList = append(depList, deployment)
+			}
+		}
+	}
+	deploymentList.Items = depList
+}
+
+func SetDeploymentStatus(ctx context.Context, client client.Client, o object.ClowdObject) error {
+	deploymentList := apps.DeploymentList{}
+	err := client.List(ctx, &deploymentList)
+
+	if err != nil {
+		return err
+	}
+
+	filterOwnedDeployments(&deploymentList, o.GetUID())
+	var managedDeployments int32
+	var readyDeployments int32
+
+	for _, deployment := range deploymentList.Items {
+		managedDeployments++
+		if ok := utils.DeploymentStatusChecker(&deployment); ok {
+			readyDeployments++
+		}
+	}
+
+	status := o.GetDeploymentStatus()
+	status.ManagedDeployments = managedDeployments
+	status.ReadyDeployments = readyDeployments
+
+	return nil
+}
