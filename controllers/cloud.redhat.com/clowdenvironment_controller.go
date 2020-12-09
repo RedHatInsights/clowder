7d5a7748da8f69fc0e764f3d5a77c534b8da20e1
Pete Savage <psavage@redhat.com>
2020-12-09 08:16:27 +0000

    Minor changes

--- controllers/cloud.redhat.com/clowdenvironment_controller.go
+++ controllers/cloud.redhat.com/clowdenvironment_controller.go
@@ -6,6 +6,7 @@ you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -100,6 +101,7 @@ func (r *ClowdEnvironmentReconciler) Reconcile(req ctrl.Request) (ctrl.Result, e
 	}
 
 	err = runProvidersForEnv(provider)
+
 	if err == nil {
 		r.Log.Info("Reconciliation successful", "env", env.Name)
 		if _, ok := managedEnvironments[env.Name]; ok == false {
