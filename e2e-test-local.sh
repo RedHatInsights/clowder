0f6bff2508363209b924781014afd29f972b8053
Pete Savage <psavage@redhat.com>
2021-01-06 18:47:14 +0000

    Create the local deployment

--- e2e-test-local.sh
+++ e2e-test-local.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-export IMAGE_TAG=`git rev-parse --short HEAD`
-
 kubectl apply -f skuttl-namespace.yaml
 kubectl apply -f skuttl-perms.yaml
 kubectl apply -f prommie-operator-bundle.yaml
 
-export IMG=$IMAGE_NAME:$IMAGE_TAG
-
-make bundle
-make docker-build-no-test
-make docker-push-local
-make deploy
+make deploy-minikube
 kubectl kuttl test --config bundle/tests/scorecard/kuttl/kuttl-test.yaml --crd-dir config/crd/bases/ bundle/tests/scorecard/kuttl/
 #operator-sdk scorecard bundle --selector=suite=kuttlsuite --verbose --namespace=skuttl-test --service-account kuttl -w 300s
