da97c87673ab287d38f8d6344d0e5db570e285d2
Pete Savage <psavage@redhat.com>
2021-01-28 19:09:05 +0000

    Added some more comments for proxy file

--- controllers/cloud.redhat.com/proxy_client.go
+++ controllers/cloud.redhat.com/proxy_client.go
@@ -16,40 +16,49 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+// ProxyClient is a type that holds mimicks a real client but tracks certain resources through
+// creation, so that they can be deleted when a ClowdApp/ClowdEnv is deleted.
 type ProxyClient struct {
 	ResourceTracker map[string]map[string]bool
 	Ctx             context.Context
 	client.Client
 }
 
+// Get proxies the Get call to the real client, running through tracking first
 func (p *ProxyClient) Get(ctx context.Context, key types.NamespacedName, obj runtime.Object) error {
 	clientOpsMetric.With(prometheus.Labels{"operation": "get"}).Inc()
 	return p.Client.Get(ctx, key, obj)
 }
 
+// Create proxies the Create call to the real client, running through tracking first
 func (p *ProxyClient) Create(ctx context.Context, obj runtime.Object, opts ...client.CreateOption) error {
 	p.AddResource(obj)
 	clientOpsMetric.With(prometheus.Labels{"operation": "create"}).Inc()
 	return p.Client.Create(ctx, obj, opts...)
 }
 
+// Update proxies the Update call to the real client, running through tracking first
 func (p *ProxyClient) Update(ctx context.Context, obj runtime.Object, opts ...client.UpdateOption) error {
 	p.AddResource(obj)
 	clientOpsMetric.With(prometheus.Labels{"operation": "update"}).Inc()
 	return p.Client.Update(ctx, obj, opts...)
 }
 
+// Patch proxies the Patch call to the real client, running through tracking first
 func (p *ProxyClient) Patch(ctx context.Context, obj runtime.Object, patch client.Patch, opts ...client.PatchOption) error {
 	p.AddResource(obj)
 	clientOpsMetric.With(prometheus.Labels{"operation": "patch"}).Inc()
 	return p.Client.Patch(ctx, obj, patch, opts...)
 }
 
+// Delete proxies the Delete call to the real client, running through tracking first
 func (p *ProxyClient) Delete(ctx context.Context, obj runtime.Object, opts ...client.DeleteOption) error {
 	clientOpsMetric.With(prometheus.Labels{"operation": "delete"}).Inc()
 	return p.Client.Delete(ctx, obj, opts...)
 }
 
+// AddResource adds the given resource to the tracking database so that it can be checked for existence
+// after the deletion of a ClowdObject.
 func (p *ProxyClient) AddResource(obj runtime.Object) {
 	log := (*p.Ctx.Value(errors.ClowdKey("log")).(*logr.Logger)).WithName("proxy-client")
 
@@ -112,6 +121,8 @@ func (p *ProxyClient) AddResource(obj runtime.Object) {
 	p.ResourceTracker[rKind][name] = true
 }
 
+// Reconcile goes through all resources in the resource tracker that still exist after a ClowdObject
+// is deleted and removes those objects. Since Clowder created them, they are assumed safe to delete.
 func (p *ProxyClient) Reconcile(uid types.UID) error {
 	log := (*p.Ctx.Value(errors.ClowdKey("log")).(*logr.Logger)).WithName("proxy-client")
 	for k := range p.ResourceTracker {
