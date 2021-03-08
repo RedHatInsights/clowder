a624711decf78db78330e127c71816a62ea5afe2
Pete Savage <psavage@redhat.com>
2021-03-08 10:57:35 +0000

    Add profiling capability

--- main.go
+++ main.go
@@ -18,6 +18,9 @@ package main
 
 import (
 	"flag"
+	"log"
+	"os"
+	"runtime/pprof"
 
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -28,8 +31,10 @@ import (
 )
 
 var setupLog = ctrl.Log.WithName("setup")
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 
 func main() {
+
 	var metricsAddr string
 	var enableLeaderElection bool
 	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
@@ -37,6 +42,15 @@ func main() {
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
 	flag.Parse()
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 
 	ctrl.SetLogger(zap.New(zap.UseDevMode(false)))
 
