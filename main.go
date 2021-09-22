0bc544dd80c56aaac28d3cc0de7b689a73ba1fa9
Kyle Lape <klape@redhat.com>
2021-09-22 10:47:45 -0500

    import pprof

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ package main
 import (
 	"flag"
 	"net/http"
+	_ "net/http/pprof"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
