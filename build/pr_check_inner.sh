b55263717b0fc46aca3444f8232ab7bd8a28b395
Blake Holifield <bholifie@redhat.com>
2021-10-18 13:51:12 -0500

    update KREW install to include OS and ARCH (#473)

--- build/pr_check_inner.sh
+++ build/pr_check_inner.sh
@@ -10,12 +10,15 @@ cd /container_workspace
 export KUBEBUILDER_ASSETS=/container_workspace/testbin/bin
 
 (
-  cd "$(mktemp -d)" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.tar.gz" &&
-  tar zxvf krew.tar.gz &&
-  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_$(uname -m | sed -e 's/x86_64/amd64/' -e 's/arm.*$/arm/')" &&
-  "$KREW" install krew
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
 )
+
 
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 
