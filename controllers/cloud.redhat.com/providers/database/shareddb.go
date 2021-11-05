f07da44e4895567a8974fd0c81be23ac8b1d158d
Pete Savage <psavage@redhat.com>
2021-11-05 18:16:36 +0000

    Add context to main

--- controllers/cloud.redhat.com/providers/database/shareddb.go
+++ controllers/cloud.redhat.com/providers/database/shareddb.go
@@ -184,7 +184,7 @@ func (db *sharedDbProvider) Provide(app *crd.ClowdApp, c *config.AppConfig) erro
 
 	appSqlConnectionString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(db.Ctx, 5*time.Second)
 	defer cancel()
 
 	dbClient, err := sql.Open("postgres", appSqlConnectionString)
