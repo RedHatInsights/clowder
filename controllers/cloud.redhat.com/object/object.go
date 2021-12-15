9a827160d20d68218c1addd956704d7bb40e6784
Pete Savage <psavage@redhat.com>
2021-12-15 12:22:03 +0000

    Optimize

--- controllers/cloud.redhat.com/object/object.go
+++ controllers/cloud.redhat.com/object/object.go
@@ -1,9 +1,12 @@
 package object
 
 import (
+	"context"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // ClowdObject is used to be able to treat ClowdEnv and ClowdApp as the same type
@@ -16,6 +19,7 @@ type ClowdObject interface {
 	GetClowdSAName() string
 	GetPrimaryLabel() string
 	GroupVersionKind() schema.GroupVersionKind
+	GetNamespacesInEnv(context.Context, client.Client) ([]string, error)
 }
 
 // LabeledClowdObject is used to be able to treat ClowdEnv and ClowdApp as the same type
