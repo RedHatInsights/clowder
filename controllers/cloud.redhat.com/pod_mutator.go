bb2cc1b5ef6482614b844e1d334003d2cadc3479
Brandon Squizzato <bsquizza@redhat.com>
2022-04-21 16:12:36 -0400

    Update caddy plugin image

--- controllers/cloud.redhat.com/pod_mutator.go
+++ controllers/cloud.redhat.com/pod_mutator.go
@@ -49,7 +49,7 @@ func (p *mutantPod) Handle(ctx context.Context, req admission.Request) admission
 			return admission.Errored(http.StatusBadRequest, fmt.Errorf("pod does not specify authsidecar config"))
 		}
 
-		image := "quay.io/cloudservices/crc-caddy-plugin:b195225"
+		image := "quay.io/cloudservices/crc-caddy-plugin:0d139d1"
 
 		if clowderconfig.LoadedConfig.Images.Caddy != "" {
 			image = clowderconfig.LoadedConfig.Images.Caddy
