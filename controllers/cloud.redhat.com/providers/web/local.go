f8c657e576b5b6ce0550fbdb1d7a5cacca676c26
Pete Savage <psav@users.noreply.github.com>
2022-04-28 11:00:57 +0100

    Revert "Calc authsidecar-confighash w/ only secret data"
    This reverts commit a3c5d8c06a607a70bfa3ebcfffd7febd6e760b39.

--- controllers/cloud.redhat.com/providers/web/local.go
+++ controllers/cloud.redhat.com/providers/web/local.go
@@ -341,7 +341,7 @@ func (web *localWebProvider) Provide(app *crd.ClowdApp, c *config.AppConfig) err
 			"whitelist":   strings.Join(deployment.WebServices.Public.WhitelistPaths, ","),
 		}
 
-		jsonData, err := json.Marshal(sec.StringData)
+		jsonData, err := json.Marshal(sec)
 		if err != nil {
 			return errors.Wrap("Failed to marshal config JSON", err)
 		}
