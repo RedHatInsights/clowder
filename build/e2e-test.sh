dc2ca16df0b081f0581ebf5b666a1ccd2acc54e9
Pete Savage <psavage@redhat.com>
2022-05-30 13:40:26 +0100

    Remove olm bundle

--- build/e2e-test.sh
+++ build/e2e-test.sh
@@ -13,5 +13,3 @@ make docker-push
 make deploy
 
 bash build/run_kuttl.sh $@  # pass any cli options to kuttl, such as "--test <test name>"
-
-#operator-sdk scorecard bundle --selector=suite=kuttlsuite --verbose --namespace=skuttl-test --service-account kuttl -w 300s
--- build/run_kuttl.sh
+++ build/run_kuttl.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-kubectl apply -f build/skuttl-namespace.yaml
-kubectl apply -f build/skuttl-perms.yaml
-
 kubectl kuttl test \
-    --config bundle/tests/scorecard/kuttl/kuttl-test.yaml \
+    --config tests/kuttl/kuttl-test.yaml \
     --manifest-dir config/crd/bases/ \
-    bundle/tests/scorecard/kuttl/ \
+    tests/kuttl/ \
     $@  # pass in any extra cmd line args that you desire, such as "--test <test name>"
