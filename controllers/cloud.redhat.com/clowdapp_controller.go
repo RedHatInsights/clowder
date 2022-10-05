0b5a5972566ec5b6a1252c7fed139cea0b21b740
Pete Savage <psavage@redhat.com>
2022-10-05 17:44:13 +0100

    Fix

--- controllers/cloud.redhat.com/clowdapp_controller.go
+++ controllers/cloud.redhat.com/clowdapp_controller.go
@@ -150,7 +150,7 @@ func (r *ClowdAppReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 	res, err := reconciliation.Reconcile()
 	if err != nil {
 		if shouldSkipReconciliation(err) {
-			log.Info("error", err.Error(), "skipping", "true", "requeue", res.Requeue)
+			log.Info("skipping", "error", err.Error(), "skipping", "true", "requeue", res.Requeue)
 			return res, nil
 		}
 		log.Error(err, "error in reconciliation", "skipping", "false", "requeue", res.Requeue)
