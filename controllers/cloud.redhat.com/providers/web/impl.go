ea2bc06053f2084e8f51e9647b4ec8241e5aef58
Stephen Adams <stephen.adams@redhat.com>
2023-01-03 13:50:44 -0500

    Change name for cert secret
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>

--- controllers/cloud.redhat.com/providers/web/impl.go
+++ controllers/cloud.redhat.com/providers/web/impl.go
@@ -112,7 +112,7 @@ func makeService(cache *rc.ObjectCache, deployment *crd.Deployment, app *crd.Clo
 	}
 
 	if deployment.WebServices.TLS {
-		var certName string = deployment.Name + "-tls-cert"
+		var certName string = app.Name + "-tls-cert"
 		annotations := map[string]string{"service.beta.openshift.io/service-cert-secret-name": certName}
 
 		utils.UpdateAnnotations(s, annotations)
