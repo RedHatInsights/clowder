49a74494ff9569d12e29ded6d582f21b5f6434f3
Victor M <vmugicag@redhat.com>
2023-03-02 05:30:15 +0100

    Update unleash image repository

--- controllers/cloud.redhat.com/providers/featureflags/provider.go
+++ controllers/cloud.redhat.com/providers/featureflags/provider.go
@@ -7,7 +7,9 @@ import (
 	p "github.com/RedHatInsights/clowder/controllers/cloud.redhat.com/providers"
 )
 
-var DefaultImageFeatureFlagsUnleash = "quay.io/cloudservices/unleash-docker:5.6.9"
+// AppSRE Unleash ver. 5.6.9
+// https://github.com/app-sre/unleash/tree/64de4f47c57e84b9838f8f1f932822212caf55fb
+var DefaultImageFeatureFlagsUnleash = "quay.io/app-sre/unleash:64de4f4"
 
 // ProvName identifies the featureflags provider.
 var ProvName = "featureflags"
