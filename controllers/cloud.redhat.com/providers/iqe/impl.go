1af4ce115dedb74dd15f6805fdd2c982bb3273fe
Sanket Jagtap <sjagtap@redhat.com>
2023-07-21 00:45:17 +0530

    Adds IQE_RP_ARGS reportportal and IQE_IBUTSU_SOURCE args to CJIs

--- controllers/cloud.redhat.com/providers/iqe/impl.go
+++ controllers/cloud.redhat.com/providers/iqe/impl.go
@@ -63,6 +63,8 @@ func createIqeContainer(j *batchv1.Job, nn types.NamespacedName, cji *crd.ClowdJ
 		{Name: "IQE_TEST_IMPORTANCE", Value: joinNullableSlice(cji.Spec.Testing.Iqe.TestImportance)},
 		{Name: "IQE_PARALLEL_ENABLED", Value: cji.Spec.Testing.Iqe.ParallelEnabled},
 		{Name: "IQE_PARALLEL_WORKER_COUNT", Value: cji.Spec.Testing.Iqe.ParallelWorkerCount},
+		{Name: "IQE_RP_ARGS", Value: cji.Spec.Testing.Iqe.RpArgs},
+		{Name: "IQE_IBUTSU_SOURCE", Value: cji.Spec.Testing.Iqe.IbutsuSource},
 	}
 
 	// set image tag
