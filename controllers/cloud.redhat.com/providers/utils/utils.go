abb54b4a777eaea32a0ee6d61973b097a7b5cdbb
Daniel Agbay <dagbay@redhat.com>
2023-08-28 15:10:31 -0500

    remove whitespace

--- controllers/cloud.redhat.com/providers/utils/utils.go
+++ controllers/cloud.redhat.com/providers/utils/utils.go
@@ -23,7 +23,7 @@ import (
 )
 
 var DefaultImageCaddySideCar = "quay.io/cloudservices/crc-caddy-plugin:1c4882e"
-var DefaultImageMBOP = "quay.io/cloudservices/mbop:a4aaa4c "
+var DefaultImageMBOP = "quay.io/cloudservices/mbop:a4aaa4c"
 var DefaultImageMocktitlements = "quay.io/cloudservices/mocktitlements:e24820c"
 var DefaultKeyCloakVersion = "15.0.2"
 var DefaultImageKeyCloak = fmt.Sprintf("quay.io/keycloak/keycloak:%s", DefaultKeyCloakVersion)
