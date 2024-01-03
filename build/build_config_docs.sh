ddf4c92fabb610738b1ef98773fa785db0a29289
Pete Savage <psavage@redhat.com>
2024-01-03 13:46:19 +0000

    Moving to new API docs generator

--- build/build_config_docs.sh
+++ build/build_config_docs.sh
@@ -2,26 +2,7 @@
 
 set -e
 
-
-if [ ! -f docs/build/node_modules/.bin/jsonschema2md ]; then
-	echo "You don't have jsonschema2md, installing it..."
-	cd docs/build/
-	if [ ! -d node_modules ]; then
-		mkdir node_modules
-	fi
-	npm install @adobe/jsonschema2md@4.2.2
-	cd -
-fi
-
-
-if [ ! -f docs/build/node_modules/.bin/jsonschema2md ]; then
-	echo "ERROR: Could not find jsonschema2md, please check installation above"
-	exit 1
-fi
-
-./docs/build/node_modules/.bin/jsonschema2md -d controllers/cloud.redhat.com/config/ -o docs/appconfig -e json
-
-rm out/schema.json
-rmdir out
-
-## Lets move to using coveooss/json-schema-for-humans
+python -m venv docs/build/venv
+source docs/build/venv/bin/activate
+pip install json-schema-for-humans==0.47
+generate-schema-doc --config template_name=md controllers/cloud.redhat.com/config/schema.json docs/api_ref.md
