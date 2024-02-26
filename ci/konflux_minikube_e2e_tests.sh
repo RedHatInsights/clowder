1e39a06212e5d2b213a97a4ff59067d9f51a3c83
Matt Knop <mknop@redhat.com>
2024-02-26 15:32:06 -0700

    minor updates

--- ci/konflux_minikube_e2e_tests.sh
+++ ci/konflux_minikube_e2e_tests.sh
@@ -2,11 +2,6 @@
 
 set -exv
 
-# copy the workspace from the Jenkins job off the ro volume into this container
-mkdir /container_workspace
-cp -r /workspace/. /container_workspace
-cd /container_workspace
-
 mkdir -p /container_workspace/bin
 cp /opt/app-root/src/go/bin/* /container_workspace/bin
 
