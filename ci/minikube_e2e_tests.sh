5567d4cb8704de863fe5abba8f84a1c1b8bc477a
Adam Drew <addrew@redhat.com>
2024-08-14 08:33:06 -0400

    Add Z to mount

--- ci/minikube_e2e_tests.sh
+++ ci/minikube_e2e_tests.sh
@@ -26,7 +26,7 @@ ls -Z ci/minikube_e2e_tests_inner.sh
 set +e
 docker run -i \
     --name $CONTAINER_NAME \
-    -v $PWD:/workspace:ro \
+    -v $PWD:/workspace:ro,Z \
     -v `$PWD/bin/setup-envtest use -p path`:/bins:ro \
     -e IMAGE_NAME=$IMAGE_NAME \
     -e IMAGE_TAG=$IMAGE_TAG \
