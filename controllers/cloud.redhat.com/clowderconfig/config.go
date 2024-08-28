49d5d142459deb30519b2e452fdc656095536f68
Matt Knop <mknop@redhat.com>
2024-08-28 12:37:18 -0600

    feat: creates feature flag to enable strimzi finalizer use

--- controllers/cloud.redhat.com/clowderconfig/config.go
+++ controllers/cloud.redhat.com/clowderconfig/config.go
@@ -47,6 +47,7 @@ type ClowderConfig struct {
 		DisableCloudWatchLogging    bool `json:"disableCloudWatchLogging"`
 		EnableExternalStrimzi       bool `json:"enableExternalStrimzi"`
 		DisableRandomRoutes         bool `json:"disableRandomRoutes"`
+		EnableStrimziFinalizerUse   bool `json:"enableStrimziFinalizerUse"`
 	} `json:"features"`
 	Settings struct {
 		ManagedKafkaEphemDeleteRegex string `json:"managedKafkaEphemDeleteRegex"`
