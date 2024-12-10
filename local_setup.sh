3aea6c0a659df9389d070ef23dfa8707785578d3
Chris Mitchell <cmitchel@redhat.com>
2024-12-10 16:35:56 -0500

    Fix dev env ref in local setup script
    Signed-off-by: Chris Mitchell <cmitchel@redhat.com>

--- local_setup.sh
+++ local_setup.sh
@@ -3,7 +3,7 @@
 minikube start --cpus 8 --disk-size 36GB --memory 16000MB --driver=kvm2 --addons registry --addons ingress  --addons=metrics-server --disable-optimizations
 # minikube start --cpus 4 --disk-size 36GB --memory 20000MB --driver=podman --addons registry --addons ingress  --addons=metrics-server --disable-optimizations
 
-~/code/RH/clowder/build/kube_setup.sh
+./build/kube_setup.sh
 
 make install
 
