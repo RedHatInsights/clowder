e549944f9d2712b7c4e785fa4d8fd9a0193616a2
Brandon Squizzato <bsquizzato@gmail.com>
2026-01-22 12:46:19 -0500

    Store .pem's in tmp dir

--- tests/kuttl/test-multi-app-interface-db/02-json-asserts.sh
+++ tests/kuttl/test-multi-app-interface-db/02-json-asserts.sh
@@ -14,29 +14,29 @@ set -x
 
 # Test commands from original yaml file
 bash -c 'for i in {1..30}; do kubectl get secret --namespace=test-multi-app-interface-db-default-ca app-default-ca && exit 0 || sleep 1; done; echo "Secret not found"; exit 1'
-curl https://truststore.pki.rds.amazonaws.com/us-east-1/us-east-1-bundle.pem > us-east-1-bundle.pem
-curl https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem > default-ca-bundle.pem
+curl https://truststore.pki.rds.amazonaws.com/us-east-1/us-east-1-bundle.pem > ${TMP_DIR}/us-east-1-bundle.pem
+curl https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem > ${TMP_DIR}/default-ca-bundle.pem
 kubectl get secret --namespace=test-multi-app-interface-db-default-ca app-default-ca -o json > ${TMP_DIR}/test-multi-app-interface-db-default-ca
 jq -r '.data["cdappconfig.json"]' < ${TMP_DIR}/test-multi-app-interface-db-default-ca | base64 -d > ${TMP_DIR}/test-multi-app-interface-db-default-ca-json
 jq -r '.database.hostname == "app-default-ca.rds.example.com"' -e < ${TMP_DIR}/test-multi-app-interface-db-default-ca-json
 jq -r '.database.sslMode == "verify-full"' -e < ${TMP_DIR}/test-multi-app-interface-db-default-ca-json
 jq -r '.database.username == "user"' -e < ${TMP_DIR}/test-multi-app-interface-db-default-ca-json
-jq -r '.database.rdsCa' < ${TMP_DIR}/test-multi-app-interface-db-default-ca-json > actual.pem
-diff --ignore-blank-lines actual.pem default-ca-bundle.pem
+jq -r '.database.rdsCa' < ${TMP_DIR}/test-multi-app-interface-db-default-ca-json > ${TMP_DIR}/actual.pem
+diff --ignore-blank-lines ${TMP_DIR}/actual.pem ${TMP_DIR}/default-ca-bundle.pem
 kubectl get secret --namespace=test-multi-app-interface-db app-c -o json > ${TMP_DIR}/test-multi-app-interface-db-c
 jq -r '.data["cdappconfig.json"]' < ${TMP_DIR}/test-multi-app-interface-db-c | base64 -d > ${TMP_DIR}/test-multi-app-interface-db-json-c
 jq -r '.database.hostname == "app-b-stage.rds.example.com"' -e < ${TMP_DIR}/test-multi-app-interface-db-json-c
 jq -r '.database.sslMode == "verify-full"' -e < ${TMP_DIR}/test-multi-app-interface-db-json-c
 jq -r '.database.username == "user"' -e < ${TMP_DIR}/test-multi-app-interface-db-json-c
-jq -r '.database.rdsCa' < ${TMP_DIR}/test-multi-app-interface-db-json-c > actual.pem
-diff --ignore-blank-lines actual.pem us-east-1-bundle.pem
+jq -r '.database.rdsCa' < ${TMP_DIR}/test-multi-app-interface-db-json-c > ${TMP_DIR}/actual.pem
+diff --ignore-blank-lines ${TMP_DIR}/actual.pem ${TMP_DIR}/us-east-1-bundle.pem
 kubectl get secret --namespace=test-multi-app-interface-db app-b -o json > ${TMP_DIR}/test-multi-app-interface-db-b
 jq -r '.data["cdappconfig.json"]' < ${TMP_DIR}/test-multi-app-interface-db-b | base64 -d > ${TMP_DIR}/test-multi-app-interface-db-json-b
 jq -r '.database.hostname == "app-b-stage.rds.example.com"' -e < ${TMP_DIR}/test-multi-app-interface-db-json-b
 jq -r '.database.sslMode == "verify-full"' -e < ${TMP_DIR}/test-multi-app-interface-db-json-b
 jq -r '.database.username == "user"' -e < ${TMP_DIR}/test-multi-app-interface-db-json-b
-jq -r '.database.rdsCa' < ${TMP_DIR}/test-multi-app-interface-db-json-c > actual.pem
-diff --ignore-blank-lines actual.pem us-east-1-bundle.pem
+jq -r '.database.rdsCa' < ${TMP_DIR}/test-multi-app-interface-db-json-c > ${TMP_DIR}/actual.pem
+diff --ignore-blank-lines ${TMP_DIR}/actual.pem ${TMP_DIR}/us-east-1-bundle.pem
 kubectl get secret --namespace=test-multi-app-interface-db app-d -o json > ${TMP_DIR}/test-multi-app-interface-db-d
 jq -r '.data["cdappconfig.json"]' < ${TMP_DIR}/test-multi-app-interface-db-d | base64 -d > ${TMP_DIR}/test-multi-app-interface-db-json-d
 jq -r '.database.hostname == "unusual.db.name.example.com"' -e < ${TMP_DIR}/test-multi-app-interface-db-json-d
