aded73ae664593ed52db074a0f18e1e3fb366efa
Fay Feghali <ffeghali@redhat.com>
2026-01-27 14:34:42 -0500

    added kuttl tests for cpu limit removal

---
+++ tests/kuttl/test-cpu-limit-removal/02-verify-no-cpu-limit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Verify that the deployment for test-app-no-cpu-limit-processor does NOT have a CPU limit
+
+# Get the CPU limit from the deployment (if it exists)
+CPU_LIMIT=$(kubectl get deployment test-app-no-cpu-limit-processor -n test-cpu-no-limit -o jsonpath='{.spec.template.spec.containers[0].resources.limits.cpu}')
+
+if [ -z "$CPU_LIMIT" ]; then
+    echo "SUCCESS: No CPU limit is set on the deployment (as expected)"
+    exit 0
+else
+    echo "FAILURE: CPU limit '$CPU_LIMIT' is set on the deployment, but it should NOT be set"
+    exit 1
+fi
